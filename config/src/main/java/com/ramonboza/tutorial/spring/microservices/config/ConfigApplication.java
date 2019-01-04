14ced79d7b45650a71a7628c0ebaec051eb10844
Ramon Boza <bozanayade@gmail.com>
2019-01-04 16:28:09 +0100

    Adding First step with config service

---
+++ config/src/main/java/com/ramonboza/tutorial/spring/microservices/config/ConfigApplication.java
@@ -0,0 +1,14 @@
+package com.ramonboza.tutorial.spring.microservices.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+}
+
